ed68671d3d787790991da803558ef8fca4ab4ab7
Tom Mitchell <tmitchel@bbn.com>
2006-03-12 20:05:25 +0000

    changing to document based, in progress

---
+++ Kaleidoscope/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Kaleidoscope
+//
+//  Created by Tom Mitchell on 3/12/06.
+//  Copyright __MyCompanyName__ 2006 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
